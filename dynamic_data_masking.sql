121d43059197ae68347cea257c349da5c11c2414
mahevarma6 <109590901+mahevarma6@users.noreply.github.com>
2022-11-16 21:45:23 +0530

    Create dynamic_data_masking.sql

---
+++ dynamic_data_masking.sql
@@ -0,0 +1,88 @@
+use role sysadmin ;
+
+create or replace database test ;
+
+--- create new warehouse
+
+
+create or replace table employee_info(employee_id number,
+                     empl_join_date date,
+                     dept varchar(10),
+                     salary number,
+                     manager_id number);
+                     
+-- insert values into employee table
+insert into  employee_info values(1,'2020-10-01','HR',40000,4),
+                                 (2,'2020-09-01','Technical',100000,9),
+                                 (3,'2020-09-01','Marketing',50000,5),
+                                 (4,'2020-09-01','HR',20000,5),
+                                 (5,'2020-09-01','HR',35000,9),
+                                 (6,'2021-09-01','Technical',90000,4),
+                                 (7,'2021-09-01','Marketing',10000,1);
+
+insert into  employee_info values
+                                 (8,'2021-09-01','R&D',20000,230);
+                                 
+                                
+                                 
+select * from  employee_info;
+
+
+USE ROLE ACCOUNTADMIN;
+
+--Currently Snowflake does not support different input output datatype for Masking Policy
+create or replace masking policy sensitive_info_masking_string as (val STRING) returns STRING ->
+  case
+    when current_role() in ('ACCOUNTADMIN') then val
+    else '****************'
+  end;
+  
+create or replace masking policy sensitive_info_masking_numbers as (val NUMBER) returns NUMBER ->
+  case
+    when current_role() in ('ACCOUNTADMIN') then (val)
+    else 9999999
+  end;
+
+  SALES_DE
+  
+  
+-- apply masking policy to a table column
+ALTER TABLE IF EXISTS employee_info MODIFY COLUMN dept SET MASKING POLICY sensitive_info_masking_string; 
+ALTER TABLE IF EXISTS employee_info MODIFY COLUMN salary SET MASKING POLICY sensitive_info_masking_numbers; 
+
+select * from employee_info ;
+ 
+
+use role SYSADMIN;
+ 
+SELECT * from employee_info;
+
+
+Create or replace view  DEMO_DB.PUBLIC.employee_set as select * from DEMO_DB.PUBLIC.employee_info;
+
+select * from employee_set;
+
+use role ACCOUNTADMIN;
+
+
+--Describe Masking Policy
+
+DESC MASKING POLICY sensitive_info_masking_numbers;
+
+
+--RECREATE MASKING POLICY
+create or replace masking policy sensitive_info_masking_numbers as (val STRING) returns STRING ->
+  case
+    when current_role() not in ('ACCOUNTADMIN') then val
+    else '****************'
+  end;
+
+--UNSET MASKING POLICY
+
+ALTER TABLE IF EXISTS employee_info MODIFY COLUMN dept UNSET MASKING POLICY;
+
+
+ALTER TABLE IF EXISTS employee_info MODIFY COLUMN dept SET MASKING POLICY sensitive_info_masking_numbers;
+
+
+
